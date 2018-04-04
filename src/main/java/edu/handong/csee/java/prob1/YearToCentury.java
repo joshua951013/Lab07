b77c81c80af7c5ecefcde48520fa6dd9aa15f1bc
Joshua <joshua951013@naver.com>
2018-04-04 16:03:16 +0900

    javadoc added

--- src/main/java/edu/handong/csee/java/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/prob1/YearToCentury.java
@@ -1,21 +1,36 @@
-package edu.handong.csee.java.prob1;
-import java.util.Scanner;
+package edu.handong.csee.java.prob1;//package name
+import java.util.Scanner;// imported the following package for the use
 
-public class YearToCentury {
+/**
+ * This class defines the YearToCentury object.
+ * It reads in the year as an input then lets the user to find out what century in belongs to.
+ * @author Josh.C
+ *
+ */
+public class YearToCentury { //a class named YearToCentury is made
 
-	int year;
+	int year;// an instance variable
 
-
-	public YearToCentury()
+/**
+ * Constructor initializing the variable 'year'
+ */
+	public YearToCentury() //constructor named YearToCentury is made
 	{
-		year = 0;
+		year = 0; //initialized the year as 0
 	}
-
-	public YearToCentury(int year)
+/**
+ * This is a method that reads in the year and puts the value into this.year
+ * @param year
+ */
+	public YearToCentury(int year)// a public type method named YearToCentury is made
 	{
-		this.year = year;
+		this.year = year;// puts the value of year to this.year 
 	}
-
+/**
+ * This is the method that reads in the value of year 
+ * then with its value, the following calculation takes place to find out which century it belongs to
+ * @return year
+ */
 	public int calc_century()
 	{
 		if(year % 100 == 0)
@@ -27,22 +42,25 @@ public class YearToCentury {
 		return year;
 	}
 	
-
-
-
-	public static void main(String [] args)
+/**
+ * This is the main method of the class.
+ * It instantiates the scanner in order to get the value of the year as an input.
+ * Then, it calls the methods mentioned above and gives the result.
+ * @param args
+ */
+	public static void main(String [] args)// main method is made
 
 	{
-		int yr = 0;
+		int yr = 0; // local variable yr is initialized as 0
 		
-		Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);// instantiating the scanner
 
-		System.out.println("Input year: ");
-		yr = keyboard.nextInt();
+		System.out.println("Input year: ");// prints out the following
+		yr = keyboard.nextInt();// reads in the value from the keyboard and puts that value into yr
 
-		YearToCentury year = new YearToCentury(yr);
+		YearToCentury year = new YearToCentury(yr);// instantiating my YearToCentury
 
-		System.out.println(yr + " is " + year.calc_century() + "th century.");
+		System.out.println(yr + " is " + year.calc_century() + "th century.");// prints out the following
 
 	}
 
