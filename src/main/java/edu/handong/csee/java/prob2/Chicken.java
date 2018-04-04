f739b836e20330684c36e64ae750fb1c043139eb
Joshua <joshua951013@naver.com>
2018-04-04 21:50:32 +0900

    added some more single line comments for prob2 and javadoc added(prob3)

--- src/main/java/edu/handong/csee/java/prob2/Chicken.java
+++ src/main/java/edu/handong/csee/java/prob2/Chicken.java
@@ -28,46 +28,46 @@ public class Chicken { // a class named chicken is made
 		this.stars = stars; //reads in the rating
 	}
 /**
- * A method returning the name of the menu
+ * A getter returning the name of the menu
  * @return name
  */
-	public String get_name(){
-		return this.name;
+	public String get_name(){ //getter is made
+		return this.name; //returns the following value
 	}
 /**
- * A method returning the price of the menu
+ * A getter returning the price of the menu
  * @return price
  */
-	public double get_price(){
-		return this.price;
+	public double get_price(){ //getter is made
+		return this.price; //returns the following value
 	}
 /**
- * A method returning the rating of the menu
+ * A getter returning the rating of the menu
  * @return stars
  */
-	public int get_stars(){
-		return this.stars;
+	public int get_stars(){ //getter is made
+		return this.stars; //returns the following value
 	}
 /**
- * A method that sets name into the following variable
+ * A setter that sets name into the following variable
  * @param name
  */
-	public void set_name(String name){
-		this.name = name;
+	public void set_name(String name){ //setter is made
+		this.name = name; //sets the name with the following
 	}
 /**
- * A method that sets price into the following variable
+ * A setter that sets price into the following variable
  * @param price
  */
-	public void set_price(double price){
-		this.price = price;
+	public void set_price(double price){ //setter is made
+		this.price = price; //sets the price with the following
 	}
 /**
- * A method that sets rating into the following variable
+ * A setter that sets rating into the following variable
  * @param stars
  */
-	public void set_stars(int stars){
-		this.stars = stars;
+	public void set_stars(int stars){ //setter is made
+		this.stars = stars; //sets the stars with the following
 	}
 /**
  * This is the main method that gives the information about the names, prices and ratings of the menu.
@@ -79,13 +79,13 @@ public class Chicken { // a class named chicken is made
 		Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 5); //instantiating chicken as menu2 giving its name, price and the rating 
 		Chicken menu3 = new Chicken("Spicey_chicken", 16000.00, 1);// instantiating chicken as menu3 giving its name, price and the rating
 
-		menu1.set_stars(3);//calls the method set.stars giving 3 as the value
-		menu2.set_stars(4);// calls the method set.stars giving 4 as the value
-		menu3.set_stars(1); //calls the method set.stars giving 1 as the value
+		menu1.set_stars(3);//calls the setter set_stars giving 3 as the value
+		menu2.set_stars(4);// calls the setter set_stars giving 4 as the value
+		menu3.set_stars(1); //calls the setter set_stars giving 1 as the value
 
-		System.out.println(menu1.get_name() + "'s rating is " + menu1.get_stars()); //prints out the following, gets the name by calling get_name method and gets the rating by get_stars
-		System.out.println(menu2.get_name() + "'s rating is " + menu2.get_stars());//prints out the following, gets the name by calling get_name method and gets the rating by get_stars
-		System.out.println(menu3.get_name() + "'s rating is " + menu3.get_stars());//prints out the following, gets the name by calling get_name method and gets the rating by get_stars
+		System.out.println(menu1.get_name() + "'s rating is " + menu1.get_stars()); //prints out the following, gets the name by calling get_name and gets the rating by get_stars
+		System.out.println(menu2.get_name() + "'s rating is " + menu2.get_stars());//prints out the following, gets the name by calling get_name and gets the rating by get_stars
+		System.out.println(menu3.get_name() + "'s rating is " + menu3.get_stars());//prints out the following, gets the name by calling get_name and gets the rating by get_stars
 	}
 
 }
--- src/main/java/edu/handong/csee/java/prob3/Attendance.java
+++ src/main/java/edu/handong/csee/java/prob3/Attendance.java
@@ -1,102 +1,147 @@
-package edu.handong.csee.java.prob3;
-import java.util.*;
-
-public class Attendance {
-	private String name = "";
-	private int year = 0;
-	private String student_id = "";
-	private int missed = 0;
-
+package edu.handong.csee.java.prob3; //package name
+import java.util.*; //imported the following class for the random case
+/**
+ * This is a class that shows whether the following students are absent or not with the number of absences they make.
+ * If they make more than 6 absences, then the program will declare that the student has failed.
+ * The number of absences will be chosen randomly from 0 to 10.
+ * @author Josh.C
+ *
+ */
+public class Attendance { //a class named Attendance is made
+	private String name = ""; //instance var
+	private int year = 0;//instance var
+	private String student_id = "";//instance var
+	private int missed = 0;//instance var
+/**
+ * Its a constructor that initializes the following variables
+ */
 	//Constructors
-	public Attendance(){
-		this.name = "Null";
-		this.year = 0;
-		this.student_id = "Null";
-		this.missed = 0;
+	public Attendance(){ //constructor attendance is made
+		this.name = "Null"; //gives name the null value
+		this.year = 0; //give year 0 value
+		this.student_id = "Null"; //gives student id the null value
+		this.missed = 0; //gives missed class as 0
 	}
 
-	public Attendance(String name, int year, String student_id, int missed){
-		this.name = name;
-		this.year = year;
-		this.student_id = student_id;
-		this.missed = missed;
+/**
+ * A method that reads in then puts those values into the following varibles
+ * @param name
+ * @param year
+ * @param student_id
+ * @param missed
+ */
+	public Attendance(String name, int year, String student_id, int missed){ //method attendance is made
+		this.name = name; //puts new name data into name
+		this.year = year; //puts new year data into year
+		this.student_id = student_id; //puts new student id data into student id
+		this.missed = missed; // puts new missed data into missed 
 	}
-	
+/**
+ * getter that returns the read value to the following
+ * @return name
+ */
 	//Getters
-	public String get_name(){
-		return name;
+	public String get_name(){ //getter is made
+		return name;//returns the following
 	}
-
-	public int get_year(){
-		return year;
+/**
+ * getter that returns the read value to the following
+ * @return year
+ */
+	public int get_year(){ //getter is made
+		return year; //returns the following
 	}
-
-	public String get_id(){
-		return student_id;
+/**
+ * getter that returns the read value to the following
+ * @return student_id
+ */
+	public String get_id(){ //getter is made
+		return student_id; //returns the following
 	}
-
-	public int get_missed(){
-		return missed;
+/**
+ * getter that returns the read value to the following
+ * @return missed
+ */
+	public int get_missed(){ //getter is made
+		return missed; //returns the following
 	}
 	
 	//Setters
-	public void set_name(String name){
-		this.name = name;
+/**
+ * setter that sets the following with the new data of it
+ * @param name
+ */
+	public void set_name(String name){// setter is made
+		this.name = name; //sets the name with the following
+	}
+/**
+ * setter that sets the following with the new data of it
+ * @param year
+ */
+	public void set_year(int year){//setter is made
+		this.year = year; //sets year with the following
+	}
+/**
+ * setter that sets the following with the new data of it
+ * @param student_id
+ */
+	public void set_id(String student_id){// setter is made
+		this.student_id = student_id; //sets student id with the following
+	}
+/**
+ * setter that sets the following with the new data of it
+ * @param missed
+ */
+	public void set_missed(int missed){ //setter is made
+		this.missed = missed; //sets missed with the following
 	}
 
-	public void set_year(int year){
-		this.year = year;
-	}
-
-	public void set_id(String student_id){
-		this.student_id = student_id;
-	}
-
-	public void set_missed(int missed){
-		this.missed = missed;
-	}
-
-
-	public static void main(String[] args) {
-		Attendance s_1 = new Attendance();
-		Attendance s_2 = new Attendance();
-		Attendance s_3 = new Attendance("Lucas", 1, "21833222", 0);
-		Attendance s_4 = new Attendance("Martha", 2, "21733444", 0);
+/**
+ * This is the main method that gives the random value from 0 to 10 which is the number of absences for the following students
+ * Then by calling the methods, setters and getter made above, it gives the values needed as the information and 
+ * shows the result whether the student is failed or not.
+ * @param args
+ */
+	public static void main(String[] args) { //main method is made
+		Attendance s_1 = new Attendance(); //instantiate Attendance as s_1
+		Attendance s_2 = new Attendance(); //instantiate Attendance as s_2
+		Attendance s_3 = new Attendance("Lucas", 1, "21833222", 0); //instantiate Attendance as s_3, giving the initial name, year(class),student id and the number of absence
+		Attendance s_4 = new Attendance("Martha", 2, "21733444", 0);//instantiate Attendance as s_4, giving the initial name, year(class),student id and the number of absence
 		
-		s_1.set_name("Jess"); s_1.set_year(4); s_1.set_id("21400999"); s_1.set_missed(0);
-		s_2.set_name("Kent"); s_2.set_year(2); s_2.set_id("21700111"); s_2.set_missed(0);
+		s_1.set_name("Jess"); s_1.set_year(4); s_1.set_id("21400999"); s_1.set_missed(0);// sets s_1's information as the following
+		s_2.set_name("Kent"); s_2.set_year(2); s_2.set_id("21700111"); s_2.set_missed(0);// sets s_2's information as the following 
 
-		Random randomGenerator = new Random();
+		Random randomGenerator = new Random(); //instantiates the random as radomGenerator
 		
-		s_1.set_missed(randomGenerator.nextInt(10));
-		s_2.set_missed(randomGenerator.nextInt(10));
-		s_3.set_missed(randomGenerator.nextInt(10));
-		s_4.set_missed(randomGenerator.nextInt(10));
+		s_1.set_missed(randomGenerator.nextInt(10)); //sets the missed number randomly from 0 to 10 of s_1
+		s_2.set_missed(randomGenerator.nextInt(10));//sets the missed number randomly from 0 to 10 of s_2
+		s_3.set_missed(randomGenerator.nextInt(10));//sets the missed number randomly from 0 to 10 of s_3
+		s_4.set_missed(randomGenerator.nextInt(10));//sets the missed number randomly from 0 to 10 of s_4
 
-		if(s_1.get_missed() > 6){
-			System.out.println("I'm sorry " + s_1.get_name() + ". You fail this course");
-			System.out.println(s_1.get_name() + " - Number of absence: " + s_1.get_missed());
+		if(s_1.get_missed() > 6){ //conditional statement for the result( if more than 6, the following result will be shown
+			System.out.println("I'm sorry " + s_1.get_name() + ". You fail this course"); //prints the following
+			System.out.println(s_1.get_name() + " - Number of absence: " + s_1.get_missed()); //prints the following
 		}
-		else
-			System.out.println("We'll see about the grade, " + s_1.get_name());
-		if(s_2.get_missed() > 6){
-			System.out.println("I'm sorry " + s_2.get_name() + ". You fail this course");
-			System.out.println(s_2.get_name() + " - Number of absence: " + s_2.get_missed());
+		else // else statement for if the missed is less than 6
+			System.out.println("We'll see about the grade, " + s_1.get_name());//prints the following
+		if(s_2.get_missed() > 6){//conditional statement for the result( if more than 6, the following result will be shown
+			System.out.println("I'm sorry " + s_2.get_name() + ". You fail this course"); //prints the following
+			System.out.println(s_2.get_name() + " - Number of absence: " + s_2.get_missed());//prints the following
 		}
-		else
+		else// else statement for if the missed is less than 6
 			System.out.println("We'll see about the grade, " + s_2.get_name());
-		if(s_3.get_missed() > 6){
-			System.out.println("I'm sorry " + s_3.get_name() + ". You fail this course");
-			System.out.println(s_3.get_name() + " - Number of absence: " + s_3.get_missed());
+		if(s_3.get_missed() > 6){//conditional statement for the result( if more than 6, the following result will be shown
+			System.out.println("I'm sorry " + s_3.get_name() + ". You fail this course");//prints the following
+			System.out.println(s_3.get_name() + " - Number of absence: " + s_3.get_missed());//prints the following
 		}
-		else
+		else// else statement for if the missed is less than 6
 			System.out.println("We'll see about the grade, " + s_3.get_name());
-		if(s_4.get_missed() > 6){
-			System.out.println("I'm sorry " + s_1.get_name() + ". You fail this course");
-			System.out.println(s_4.get_name() + " - Number of absence: " + s_4.get_missed());
+		if(s_4.get_missed() > 6){//conditional statement for the result( if more than 6, the following result will be shown
+			System.out.println("I'm sorry " + s_1.get_name() + ". You fail this course");//prints the following
+			System.out.println(s_4.get_name() + " - Number of absence: " + s_4.get_missed());//prints the following
 		}
-		else
-			System.out.println("We'll see about the grade, " + s_4.get_name());
+		else// else statement for if the missed is less than 6
+			System.out.println("We'll see about the grade, " + s_4.get_name());//prints the following
 
 	}
 
